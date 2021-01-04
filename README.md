47f848e129f2c2ea9c9e92a4f2245f2b581c904a
sazid-khan16 <74134001+sazid-khan16@users.noreply.github.com>
2021-01-04 13:45:03 +0530

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# PROJECT-_-20
+# p5.play-boilerplate
+Boiler plate for p5.play
---
+++ sketch.js
@@ -0,0 +1,37 @@
+var car;
+var wall;
+var weight,speed;
+
+function setup() {
+  createCanvas(1600,400);
+  speed=random(55,90);
+  weight=random(400,1500)
+
+  car = createSprite(50,200,50,50);
+  car.velocitiX=speed;
+  car.shapeColour=colour(255);
+
+  wall = createSprite(1500,200,60,height/2);
+  wall.shapeColour=colour(80,80,80);
+}
+
+function draw() {
+  if(wall.x-car.x<(car.width+wall.width)/2)
+  {
+    car.velocityX=0;
+    var Deformation=0.5*weight*speed*speed/22500;
+    if(Deformation>180)
+    {
+      car.shapeColour=colour(255,0,0);
+    }
+    if(Deformation<180 && Deformation>100)
+    {
+      car.shapeColour=colour(230,230,0);
+    }
+    if(Deformation<100)
+    {
+      car.shapeColour=colour(0,255,0);
+    }
+  }
+  drawSprites();
+}
